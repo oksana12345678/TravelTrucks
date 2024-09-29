9f55f380cbea0b5ff206b47c13b716a757df1272
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-09-29 22:36:54 +0200

    fix

--- src/components/CamperComponents/CamperPhotos/CamperPhotos.styled.js
+++ src/components/CamperComponents/CamperPhotos/CamperPhotos.styled.js
@@ -20,6 +20,7 @@ export const SwiperStyle = styled(Swiper)`
 export const ImageStyle = styled.img`
   width: 100%;
   height: 100%;
+  object-fit: cover;
   display: block;
   border-radius: 10px;
 `;
